1cc4d0c1c59a26d177624ce471962a0f940aa7f1
John McKim <johncmckim@gmail.com>
2016-07-04 06:29:50 +1000

    Update api gateway terraform

--- apex/functions/foo/foo.js
+++ apex/functions/foo/foo.js
@@ -5,7 +5,8 @@ export default {
 
     cb(null, {
       id: id,
-      type: 'foo'
+      type: 'foo',
+      location: '/foo/' + id
     });
   },
   put(id, name, cb) {
@@ -14,15 +15,19 @@ export default {
     cb(null, {
       id: id,
       name: name,
-      type: 'updated-foo'
+      type: 'updated-foo',
+      location: '/foo/' + id
     });
   },
   post(name, cb) {
     console.log('POST', name);
+    var id = Math.floor(Math.random() * 100) + 1;
 
     cb(null, {
+      id: id,
       name: name,
-      type: 'created-foo'
+      type: 'created-foo',
+      location: '/foo/' + id
     });
   }
 }
